57b103ec965c95691ddfe8d8c265b64ac4f0d37b
IbragimKhaniev <newinterny@gmail.com>
2025-12-02 22:45:43 +0300

    fix: add missing Feedback model import in api/views.py

--- api/views.py
+++ api/views.py
@@ -2,6 +2,8 @@ from rest_framework.generics import CreateAPIView
 
 from .serializers import FeedbackSerializer
 
+from .models import Feedback
+
 
 class FeedbackCreateView(CreateAPIView):
     queryset = Feedback.objects.all()
