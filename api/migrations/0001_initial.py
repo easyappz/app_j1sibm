c0db33fff7833976e4d73a86e31981ec64a69316
IbragimKhaniev <newinterny@gmail.com>
2025-12-02 22:49:58 +0300

    fix(api/migrations/0001_initial.py): replace min_length with MinLengthValidator and fix validator imports

--- api/migrations/0001_initial.py
+++ api/migrations/0001_initial.py
@@ -1,5 +1,5 @@
 from django.db import migrations, models
-import django.core.validators
+from django.core.validators import MinLengthValidator, RegexValidator
 
 
 class Migration(migrations.Migration):
@@ -13,8 +13,8 @@ class Migration(migrations.Migration):
             name='Feedback',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=255, min_length=1)),
-                ('phone', models.CharField(max_length=20, validators=[django.core.validators.RegexValidator(regex=r'^\+?[7|8][ \\-]?[0-9]{10,}$')])),
+                ('name', models.CharField(max_length=255, validators=[MinLengthValidator(1)])),
+                ('phone', models.CharField(max_length=20, validators=[RegexValidator(regex=r'^\\+?[7|8][ \\-]?[0-9]{10,}$')])),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
             ],
             options={
