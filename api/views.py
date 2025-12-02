ef8c680c563ec8ec941b101cc34b9795f59c27d8
IbragimKhaniev <newinterny@gmail.com>
2025-12-02 23:40:44 +0300

    fix: CSRF exempt for public /api/feedback/ POST + update feedback autotests

--- api/views.py
+++ api/views.py
@@ -1,10 +1,14 @@
 from rest_framework.generics import CreateAPIView
 
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+
 from .serializers import FeedbackSerializer
 
 from .models import Feedback
 
 
+@method_decorator(csrf_exempt, name='dispatch')
 class FeedbackCreateView(CreateAPIView):
     queryset = Feedback.objects.all()
     serializer_class = FeedbackSerializer
